f76d1db361c41d4c9864f02a358408eafd5d722c
Navish Sharma <navishsharma2004@gmail.com>
2025-07-18 07:45:35 +0000

    Configure the home controller to include the new OMDB and Gemini model functions and parse the data to the default view.

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,10 +4,21 @@ class Home extends Controller {
 
     public function index() {
       $user = $this->model('User');
-      $data = $user->test();
-       
-      $_SESSION['current_page'] = 'home';
-	    $this->view('home/index');
+      $omdb = $this->model('OMDB');
+      $ai = $this->model('Gemini');
+      
+      $data_viral = $ai->generateTrendingMovies();
+      $data_upcoming = $ai->generateUpcomingMovies();
+      echo "<script>console.log(" . json_encode($data_viral) . ");</script>";
+      echo "\n<script>console.log(" . json_encode($data_upcoming) . ");</script>";
+      
+      $viral_movies = $omdb->getMovies($data_viral);
+      $upcoming_movies = $omdb->getMovies($data_upcoming);
+      
+	    $this->view('home/index', [
+                  'viral_movies' => $viral_movies,
+                  'upcoming_movies' => $upcoming_movies
+      ]);
 	    die;
     }
 
