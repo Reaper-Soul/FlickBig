58b5e8451906f0d2b152e4d66b28ac39070445bd
Navish Sharma <navishsharma2004@gmail.com>
2025-07-21 06:10:28 +0000

    Updated the routing logic to handle server AJAX calls

--- app/core/App.php
+++ app/core/App.php
@@ -8,60 +8,59 @@ class App {
     protected $params = [];
 
     public function __construct() {
-        // This will return a broken up URL
-        // it will be /controller/method
         $url = $this->parseUrl();
 
-        /* if controller exists in the URL, then go to it
-         * if not, then go to this->controller which is defaulted to home 
-         */
-
-        if (file_exists('app/controllers/' . $url[1] . '.php')) {
+        // Load controller if exists
+        if (isset($url[1]) && file_exists('app/controllers/' . $url[1] . '.php')) {
             $this->controller = $url[1];
-
             $_SESSION['controller'] = $this->controller;
             $_SESSION['current_page'] = $this->controller;
 
-            /* This is if we have a special URL in the index.
-             * For example, our apply page is public and in the index method
-             * We do not want the method to be login in this case, but instead index
-             * 
-             */
-            if (in_array($this->controller, $this->special_url)) { 
-              $this->method = 'index';
+            if (in_array($this->controller, $this->special_url)) {
+                $this->method = 'index';
             }
+
             unset($url[1]);
-        } else {
-            header('Location: /home');
-            die;
+        } elseif (!file_exists('app/controllers/' . $this->controller . '.php')) {
+            // If nothing matches, redirect to /home
+            if (empty($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+                header('Location: /home');
+                exit;
+            } else {
+                header('Content-Type: application/json');
+                http_response_code(404);
+                echo json_encode(['error' => 'Controller not found']);
+                exit;
+            }
         }
 
         require_once 'app/controllers/' . $this->controller . '.php';
-
         $this->controller = new $this->controller;
 
-        // check to see if method is passed
-        // check to see if it exists
-        if (isset($url[2])) {
-            if (method_exists($this->controller, $url[2])) {
-                $this->method = $url[2];
-                $_SESSION['method'] = $this->method;
-                unset($url[2]);
-            }
+        // Load method if exists
+        if (isset($url[2]) && method_exists($this->controller, $url[2])) {
+            $this->method = $url[2];
+            $_SESSION['method'] = $this->method;
+            unset($url[2]);
         }
 
-        // This will rebase the params to a new array (starting at 0)
-        // if params exist
         $this->params = $url ? array_values($url) : [];
-        call_user_func_array([$this->controller, $this->method], $this->params);		
+
+        try {
+            call_user_func_array([$this->controller, $this->method], $this->params);
+        } catch (Throwable $e) {
+            header('Content-Type: application/json');
+            http_response_code(500);
+            echo json_encode(['error' => $e->getMessage()]);
+            exit;
+        }
     }
 
     public function parseUrl() {
-        $u = "{$_SERVER['REQUEST_URI']}";
-        // trims the trailing forward slash (rtrim), sanitizes URL, explode it by forward slash to get elements
+        $u = $_SERVER['REQUEST_URI'];
         $url = explode('/', filter_var(rtrim($u, '/'), FILTER_SANITIZE_URL));
-		unset($url[0]);
-		return $url;
+        unset($url[0]);
+        return $url;
     }
 
 }
