9947f38274ed8e881dfd862b806668150eae7331
Navish Sharma <navishsharma2004@gmail.com>
2025-07-23 11:46:53 +0000

    removed unwanted script

--- app/views/templates/components/alert.php
+++ app/views/templates/components/alert.php
@@ -15,11 +15,6 @@ if (isset($_SESSION['alert'])) {
     </div>
 <?php endif; ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-  crossorigin="anonymous">
-</script>
-
 <script>
     setTimeout(() => {
         const alert = document.querySelector('.alert');
