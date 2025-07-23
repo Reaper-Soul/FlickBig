a65bddf65349f79cc48482049fdbf811a48f80aa
Navish Sharma <navishsharma2004@gmail.com>
2025-07-23 11:48:33 +0000

    left for a future update

--- app/views/watchlist/index.php
+++ app/views/watchlist/index.php
@@ -1,3 +1,5 @@
 <?php require_once 'app/views/templates/header.php' ?>
-
+<div class="container vh-100 vw-100 align-items-center justify-content-center text-white d-flex">
+    <h1>Work in Progress! ⚒️</h1>
+</div>
 <?php require_once 'app/views/templates/footer.php' ?>
