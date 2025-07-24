f2467f4665d5b8e0a6fbb18618bda50fea711a4c
Navish Sharma <navishsharma2004@gmail.com>
2025-07-24 20:05:13 +0000

    Reduced the load time by hard-coding the movie titles for now.

--- app/models/Gemini.php
+++ app/models/Gemini.php
@@ -11,67 +11,83 @@ class Gemini{
 
   public function generateTrendingMovies(){
 
-    $data = [
-        "contents" => [[
-            "parts" => [[
-                "text" => "Note: Remember just list the titles without saying anything else in the response. There should not be any spaces at the start of the movie title. No formatting, no indexing or anything else. It should be real movies not made up names.Give me a list of 10 currently trending movies with their exact official titles."
-            ]]
-        ]]
-    ];
+    // $data = [
+    //     "contents" => [[
+    //         "parts" => [[
+    //             "text" => "Note: Remember just list the titles without saying anything else in the response. There should not be any spaces at the start of the movie title. No formatting, no indexing or anything else. It should be real movies not made up names.Give me a list of 10 currently trending movies with their exact official titles."
+    //         ]]
+    //     ]]
+    // ];
 
-    $ch = curl_init($this->url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+    // $ch = curl_init($this->url);
+    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    // curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
 
-    $resp = curl_exec($ch);
-    curl_close($ch);
+    // $resp = curl_exec($ch);
+    // curl_close($ch);
 
-    if (!$resp){
-      return null;
-    }
+    // if (!$resp){
+    //   return null;
+    // }
 
-    $json = json_decode($resp, true);
+    // $json = json_decode($resp, true);
 
-    if (isset($json['candidates'][0]['content']['parts'][0]['text'])) {
-        $text = $json['candidates'][0]['content']['parts'][0]['text'];
-        $result = array_filter(array_map('trim', explode("\n", $text)));
-        return $result;
-    }
+    // if (isset($json['candidates'][0]['content']['parts'][0]['text'])) {
+    //     $text = $json['candidates'][0]['content']['parts'][0]['text'];
+    //     $result = array_filter(array_map('trim', explode("\n", $text)));
+    //     return $result;
+    // }
 
-    return null;
+    // return null;
+      return [
+        "Oppenheimer",
+        "Pulp Fiction",
+        "The Godfather",
+        "Killers of the Flower Moon",
+        "Loki",
+        "Lord of the Rings",
+        "The Conjuring",
+      ];
   }
 
     public function generateUpcomingMovies(){
-        $data = [
-            "contents" => [[
-                "parts" => [[
-                    "text" => "Note: Remember just list the titles without saying anything else in the response. There should not be any spaces at the start of the movie title. No formatting, no indexing or anything else. It should be real movies not made up names. Make sure that none of these movies are released as of the year 2025 which means these movies should be of year 2026 or after. Give me a list of 10 upcoming movies which means the ones which are not yet released in the theatres, along with their exact official titles."
-                ]]
-            ]]
+        // $data = [
+        //     "contents" => [[
+        //         "parts" => [[
+        //             "text" => "Note: Remember just list the titles without saying anything else in the response. There should not be any spaces at the start of the movie title. No formatting, no indexing or anything else. It should be real movies not made up names. Make sure that none of these movies are released as of the year 2025 which means these movies should be of year 2026 or after. Give me a list of 10 upcoming movies which means the ones which are not yet released in the theatres, along with their exact official titles."
+        //         ]]
+        //     ]]
+        // ];
+
+        // $ch = curl_init($this->url);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+
+        // $resp = curl_exec($ch);
+        // curl_close($ch);
+
+        // if (!$resp){
+        //   return null;
+        // }
+
+        // $json = json_decode($resp, true);
+
+        // if (isset($json['candidates'][0]['content']['parts'][0]['text'])) {
+        //     $text = $json['candidates'][0]['content']['parts'][0]['text'];
+        //     $result = array_filter(array_map('trim', explode("\n", $text)));
+        //     return $result;
+        // }
+
+        // return null;
+        return [
+          "Tron: Ares",
+          "Toy Story 5",
+          "The Batman Part II",
+          "Avatar: Fire and Ash",
+          "Now You See Me: Now You Don't"
         ];
-
-        $ch = curl_init($this->url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
-
-        $resp = curl_exec($ch);
-        curl_close($ch);
-
-        if (!$resp){
-          return null;
-        }
-
-        $json = json_decode($resp, true);
-
-        if (isset($json['candidates'][0]['content']['parts'][0]['text'])) {
-            $text = $json['candidates'][0]['content']['parts'][0]['text'];
-            $result = array_filter(array_map('trim', explode("\n", $text)));
-            return $result;
-        }
-
-        return null;  
     }
 
     public function generateReview($movieTitle){
